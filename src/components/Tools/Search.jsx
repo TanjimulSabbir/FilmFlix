47d3e97906c8b6817f90780bb9d29cdb772db903
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-14 01:57:38 +0600

    font adding

--- src/components/Tools/Search.jsx
+++ src/components/Tools/Search.jsx
@@ -1,11 +1,9 @@
 import style from "../style/search.module.css"
 function Search() {
     return (
-        <div>
-            <div className="relative mt-10">
-                <input className="w-full rounded-3xl py-3 border pl-5" type="text" placeholder="Search for a movie, tv show, person..." />
-                <button className={`${style.allSearchBtn}`}>Search</button>
-            </div>
+        <div className="relative mt-10">
+            <input className="w-full rounded-3xl py-3 pl-5 border outline-none placeholder:text-gray-500" type="text" placeholder="Search for a movie, tv show, person......" />
+            <button className={`${style.allSearchBtn}`}>Search</button>
         </div>
     )
 }
--- src/components/accessories/Toggle.jsx
+++ src/components/accessories/Toggle.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import style from "../components/style/Toggle.module.css"
+import style from "../style/Toggle.module.css"
 
 export default function Toggle({ data, type }) {
     const [slectToggle, setSelectToggle] = useState(data[0].title);
@@ -7,11 +7,11 @@ export default function Toggle({ data, type }) {
         setSelectToggle(item.title)
     }
     return (
-        <div className="flex items-center space-x-5 mt-10 mb-6 text-black">
+        <div className="flex items-center space-x-5 mt-10 mb-6 text-base">
             <h1 className="text-lg font-bold">{type}</h1>
-            <div className="flex items-center justify-between border border-black rounded-3xl">
+            <div className="flex items-center justify-between border border-gray-600 rounded-3xl">
                 {data?.map(item => (
-                    <p key={item.title} className={`cursor-pointer py-[3px] px-4 ${(slectToggle === item.title) ? style.Toggled:"text-black"}`} onClick={() => handleToggle(item)}>
+                    <p key={item.title} className={`cursor-pointer py-[3px] px-6 text-base ${(slectToggle === item.title) ? style.Toggled:"text-black"}`} onClick={() => handleToggle(item)}>
                         {item.title}</p>
                 ))}
             </div>
