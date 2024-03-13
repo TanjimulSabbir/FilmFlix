e10749649d47151911de39dba712640d5f0414ee
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-14 02:01:43 +0600

    font adding

--- src/components/accessories/Toggle.jsx
+++ src/components/accessories/Toggle.jsx
@@ -8,7 +8,7 @@ export default function Toggle({ data, type }) {
     }
     return (
         <div className="flex items-center space-x-5 mt-10 mb-6 text-base">
-            <h1 className="text-lg font-bold">{type}</h1>
+            <h1 className="text-[22px] tracking-wider font-semibold">{type}</h1>
             <div className="flex items-center justify-between border border-gray-600 rounded-3xl">
                 {data?.map(item => (
                     <p key={item.title} className={`cursor-pointer py-[3px] px-6 text-base ${(slectToggle === item.title) ? style.Toggled:"text-black"}`} onClick={() => handleToggle(item)}>
