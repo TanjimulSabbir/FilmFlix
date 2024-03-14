8fc14b6de3627379fa6efaa6b1272dc0ba7a2acb
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-15 00:38:26 +0600

    working on Home page

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    // https://api.themoviedb.org/3/movie/now_playing?api_key=95735e862c8d7543ceee5364740d5db4
+    // https://image.tmdb.org/t/p/original/8uVKfOJUhmybNsVh089EqLHUYEG.jpg
+    
+    return (
+        <div>Introduction</div>
+    )
+}
+
+export default Home;
