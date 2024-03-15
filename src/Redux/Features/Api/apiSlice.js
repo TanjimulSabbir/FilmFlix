79b221c2034a20114438805c8e76015580dd73b7
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-16 03:11:43 +0600

    working on api

--- src/Redux/Features/Api/apiSlice.js
+++ src/Redux/Features/Api/apiSlice.js
@@ -1,11 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_KEY = "95735e862c8d7543ceee5364740d5db4";
+export const API_KEY = "api_key=95735e862c8d7543ceee5364740d5db4";
+const PRIMARY_BASE_URL = "https://api.themoviedb.org/3"
 
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: `https://api.themoviedb.org/3?api_key=${API_KEY}`,
+        baseUrl: PRIMARY_BASE_URL,
         // prepareHeaders(headers) {
         //     // Add your API key to the headers
         //     headers.set("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NTczNWU4NjJjOGQ3NTQzY2VlZTUzNjQ3NDBkNWRiNCIsInN1YiI6IjY1NWI2OTYxZDRmZTA0MDBjNDI1MTJmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JsI5fulRuyIsdLw4ngiBKEbWtUGPAyd_42qiZgaHVb8");
@@ -16,4 +17,4 @@ export const apiSlice = createApi({
     endpoints: (builder) => ({})
 });
 
-// baseUrl: import.meta.env.REACT_APP_PRIMARY_BASE_URL + "?apiKey=" + import.meta.env.REACT_APP_API_KEY
+// process.env.REACT_APP_PRIMARY_BASE_URL
--- src/Redux/Features/Api/movieApi.js
+++ src/Redux/Features/Api/movieApi.js
@@ -1,10 +1,10 @@
-import { apiSlice } from "./apiSlice";
+import { API_KEY, apiSlice } from "./apiSlice";
 
 export const movieApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getMovies: builder.query({
             query: (type) => ({
-                url: `/movie/now_playing`,
+                url: `/movie/now_playing?${API_KEY}`,
             })
         })
     })
