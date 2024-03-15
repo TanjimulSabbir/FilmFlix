8872b53140bc72c1ef0d5adb147bcd00329aa02c
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-15 07:18:33 +0600

    working on carousel

--- src/Layout/LandingPage.jsx
+++ src/Layout/LandingPage.jsx
@@ -14,6 +14,8 @@ function LandingPage() {
     waitForAnimate: false,
     autoplay: true,
     autoplaySpeed: 4000,
+    pauseOnHover: true,
+    arrows: false
   };
 
 
@@ -26,12 +28,12 @@ function LandingPage() {
         <iframe src="https://www.youtube.com/embed/_YUzQa_1RCE?autoplay=1&mute=0&loop=1&color=white&controls=0&modestbranding=1&playsinline=1&rel=0&enablejsapi=1&playlist=_YUzQa_1RCE" title="YouTube video player" frameBorder="1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
 
-      <div className="slider-container">
+      <div className="slider-container w-24">
         <Slider {...settings}>
-          <div className="bg-green-400 w-20 h-20 text-center">box 1</div>
-          <div className="bg-green-400 w-20 h-20 text-center">box 2</div>
-          <div className="bg-green-400 w-20 h-20 text-center">box 3</div>
-          <div className="bg-green-400 w-20 h-20 text-center">box 4</div>
+          <div className="bg-green-400 max-w-20 h-20 text-center absolute m-3"><p>box 1</p></div>
+          <div className="bg-green-400 max-w-20 h-20 text-center  m-3">box 2</div>
+          <div className="bg-green-400 max-w-20 h-20 text-center  m-3">box 3</div>
+          <div className="bg-green-400 max-w-20 h-20 text-center  m-3">box 4</div>
 
         </Slider>
       </div>
