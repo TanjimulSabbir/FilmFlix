4ec9915c11db1213e90d7e56a8d84faaeb53cf62
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-22 04:50:02 +0600

    working on trailer videos:fonts

--- src/components/Home/BannerText02.jsx
+++ src/components/Home/BannerText02.jsx
@@ -1,7 +1,7 @@
 import { CiPlay1 } from "react-icons/ci";
 import moment from "moment";
 import Trailer from "./Trailer";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import style from "../../style/bannerText.module.css";
 import toast from "react-hot-toast";
 import { useDispatch } from "react-redux";
@@ -9,31 +9,33 @@ import { getSliderValue } from "../../Redux/Features/Slider/sliderSlice";
 
 function BannerText02({ movie }) {
     const [openTrailer, setOpenTrailer] = useState(false);
-    const monthName = moment().month(parseInt(movie?.release_date?.split('-')[1]) - 1).format('MMMM');
+    const monthName = moment(movie?.release_date).format('MMMM');
     const dispatch = useDispatch();
 
+    useEffect(() => {
+        console.log(openTrailer, "from Banner");
+    }, [openTrailer]);
+
     const handleTrailer = () => {
-        setOpenTrailer(!openTrailer)
-        dispatch(getSliderValue(openTrailer))
-        toast.success(!openTrailer ? "Trailer opened" : "Trailer closed");
+        const newTrailerState = !openTrailer;
+        setOpenTrailer(newTrailerState);
+        dispatch(getSliderValue(newTrailerState));
+        toast.success(newTrailerState ? "Trailer opened" : "Trailer closed");
     };
 
-
-    console.log(openTrailer, "from Banner");
     return (
-        <div className="w-1/2 flex justify-end">
+        <div className="w-1/2 flex items-center justify-end">
             <div>
-                <h1 className={`${style.theatePlayDate} leading-none`}>
-                    <span className="mr-5">on</span>
-                    <span>
-                        <span>{movie?.release_date?.split('-')[2]}</span>
-                        <span className="-ml-1">th</span>
-                    </span>
+                <h1 className={`${style.theatePlayDate} leading-none flex flex-col items-center justify-center`}>
+                   <div>
+                   <span className="mr-5 tracking-wider">on</span>
+                    <span>{moment(movie?.release_date).format('Do')}</span>
                     <br />
-                    <span className="">{monthName}</span>
+                    <span className="tracking-widest">{monthName}</span>
+                   </div>
                 </h1>
 
-                <div className="mt-14">
+                <div className="mt-10 flex items-center justify-center">
                     <button className={`flex items-center space-x-2`} onClick={handleTrailer}>
                         <CiPlay1 className="text-3xl text-green-600" />
                         <span className="text-lg">
