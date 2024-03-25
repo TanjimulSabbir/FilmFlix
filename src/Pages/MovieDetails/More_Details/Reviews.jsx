e2230c7a1cd6f7ea56a2eb6796ae9987156c6e34
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-25 20:36:56 +0600

    working on reviews page:mostly completed

--- src/Pages/MovieDetails/More_Details/Reviews.jsx
+++ src/Pages/MovieDetails/More_Details/Reviews.jsx
@@ -8,31 +8,23 @@ import { useEffect, useState } from "react";
 
 export default function Reviews() {
   const movie = useSelector(state => state.movieData.clickedMovieDetails);
-  const [intialReviewsData, setIntialReviewsData] = useState([]);
   const [loadMore, setLoadMore] = useState(4);
 
   const { id, adult, backdrop_path, belongs_to_collection, budget, genres, homepage, imdb_id, original_language, original_title, overview, popularity, poster_path, production_companies, production_countries, release_date, revenue, runtime, spoken_languages, status, tagline, title, video, vote_average, vote_count
   } = movie || {};
 
+  const intialReviewsData = useGetData({ type: "movie", id, keyword: "reviews" });
 
   const handleLoadMoreData = () => {
-    const remainingReviews = Math.max(intialReviewsData?.length - loadMore, 0);
+    const remainingReviews = Math.max(intialReviewsData?.results?.length - loadMore, 0);
     const nextLoad = Math.min(remainingReviews, 4);
     setLoadMore((prevLoadMore) => prevLoadMore + nextLoad);
   }
 
-  const reviewsData = intialReviewsData?.slice(0, loadMore);
+  const reviewsData = intialReviewsData?.results?.slice(0, loadMore);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await useGetData({ type: "movie", id, keyword: "reviews" });
-      setIntialReviewsData(data.results)
-    };
-    fetchData();
 
-  }, [])
-
-  console.log(reviewsData, "reviewsData");
+  console.log(intialReviewsData?.results?.length, loadMore, "intialReviewsData?.result?.length > loadMore ");
 
   return (
     <div className="">
@@ -56,8 +48,8 @@ export default function Reviews() {
       }
 
       <p
-        className="flex items-center justify-center w-1/2 mx-auto space-x-1 bg-gray-500 rounded-3xl px-10 py-2 text-xl mt-9 cursor-pointer"
-        onClick={() => handleLoadMoreData()}
+        className={`${intialReviewsData?.total_results > loadMore ? 'cursor-pointer' : 'cursor-not-allowed bg-gray-400'} flex items-center justify-center w-1/2 mx-auto space-x-1 bg-gray-700 rounded-3xl px-10 py-2 text-xl mt-9`}
+        onClick={handleLoadMoreData}
       >
         <span>More audience reviews</span>
         <PiArrowRight />
