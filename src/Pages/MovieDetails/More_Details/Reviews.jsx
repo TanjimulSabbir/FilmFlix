6bb7ba1b8a1d8d87036973a6d1952bd401adbcf0
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-25 20:01:17 +0600

    working on reviews page:mostly completed

--- src/Pages/MovieDetails/More_Details/Reviews.jsx
+++ src/Pages/MovieDetails/More_Details/Reviews.jsx
@@ -8,27 +8,31 @@ import { useEffect, useState } from "react";
 
 export default function Reviews() {
   const movie = useSelector(state => state.movieData.clickedMovieDetails);
+  const [intialReviewsData, setIntialReviewsData] = useState([]);
   const [loadMore, setLoadMore] = useState(4);
 
   const { id, adult, backdrop_path, belongs_to_collection, budget, genres, homepage, imdb_id, original_language, original_title, overview, popularity, poster_path, production_companies, production_countries, release_date, revenue, runtime, spoken_languages, status, tagline, title, video, vote_average, vote_count
   } = movie || {};
 
-  const intialReviewsData = useGetData({ type: "movie", id, keyword: "reviews" });
 
-  // const handleLoadMoreData = () => {
-  //   const initialLength = intialReviewsData?.results?.length >= loadMore + 4;
+  const handleLoadMoreData = () => {
+    const remainingReviews = Math.max(intialReviewsData?.length - loadMore, 0);
+    const nextLoad = Math.min(remainingReviews, 4);
+    setLoadMore((prevLoadMore) => prevLoadMore + nextLoad);
+  }
 
-  //   setLoadMore((previous) => initialLength ? previous + 4 : previous)
-  // }
+  const reviewsData = intialReviewsData?.slice(0, loadMore);
 
-  // const reviewsData = intialReviewsData?.results?.slice(0, loadMore);
-  const remainingReviews = Math.max(intialReviewsData?.results?.length - loadMore, 0);
-  const nextLoad = Math.min(remainingReviews, 4); // Load next 4 reviews or less if there are fewer remaining
-  setLoadMore((prevLoadMore) => prevLoadMore + nextLoad); // Increment loadMore state
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await useGetData({ type: "movie", id, keyword: "reviews" });
+      setIntialReviewsData(data.results)
+    };
+    fetchData();
 
-  console.log(reviewsData, "reviewsData")
-  useEffect(() => { }, [loadMore])
+  }, [])
+
+  console.log(reviewsData, "reviewsData");
 
   return (
     <div className="">
