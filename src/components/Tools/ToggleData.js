574456eb4d2ab148b6f3032741692660eab2cef0
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-26 09:32:42 +0600

    working on similar:videos height problem solved

--- src/components/Tools/ToggleData.js
+++ src/components/Tools/ToggleData.js
@@ -6,16 +6,20 @@ const ToggleData = () => {
     const DaysLater = (days) => useMemo(() => moment().add(days, 'days').format('YYYY-MM-DD'), [days]);
 
     const toggle01 = [
-        { title: 'In Theaters', path: 'with_release_type=2|3', type: "movie" },
-        { title: 'Popular', path: 'popular', type: "movie" },
+        { title: 'In Theaters', type: "movie", keyword: "top_rated" },
+        { title: 'Popular', type: "movie", keyword: "popular" },
     ];
 
     const toggle02 = [
         { title: 'Today', path: `primary_release_date.gte=${currentDate}&primary_release_date.lte=${DaysLater(1)}`, type: "movie" },
         { title: 'This week', path: `primary_release_date.gte=${currentDate}&primary_release_date.lte=${DaysLater(6)}`, type: "movie" },
     ];
+    const toggle03 = [
+        { title: 'Upcoming', type: "movie", keyword: "upcoming" },
+        { title: 'Now Playing', type: "movie", keyword: "now_playing" },
+    ];
 
-    return { toggle01, toggle02 };
+    return { toggle01, toggle02, toggle03 };
 };
 
 export default ToggleData;
