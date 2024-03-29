4f92e521f8ff8af1708036bf4b0ed3f7e66a56a3
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-30 03:04:48 +0600

    working on movies:filter UI

--- src/Pages/Movies/MoviesHome.jsx
+++ src/Pages/Movies/MoviesHome.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import toast from "react-hot-toast";
+import { FaMinus, FaPlus } from "react-icons/fa6";
 import { useSelector } from "react-redux";
 
 export default function MoviesHome() {
   const genres = useSelector(state => state.movieData.genresData.map(genre => genre.name));
   const [openSections, setOpenSections] = useState([]);
+  const [btnOpen, setBtnOpen] = useState([]);
 
   const toggleSection = ({ keywordTitle, index }) => {
     setOpenSections(prevItems => {
@@ -29,22 +30,26 @@ export default function MoviesHome() {
     { "keywordTitle": "IMDb Rating", "keywords": ["9.0 or Higher", "8.0 or Higher", "7.0 or Higher", "6.0 or Higher", "5.0 or Higher"] }
   ];
 
+  const InputTypes = ["Genres"]
+
   return (
     <div className="pt-24 container mx-auto">
       {genresData.map((item, index) => (
         <div key={item.keywordTitle} className="relative">
-          <button onClick={() => toggleSection({ keywordTitle: item.keywordTitle, index })} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 mt-20">
-            {item.keywordTitle}
-          </button>
-          {openSections.find(section => section.keywordTitle === item.keywordTitle && section.index) && (
-            <div className="transition-all flex flex-col space-y-1 ease-in-out duration-500 absolute w-full left-0 mt-2">
-              {item.keywords.map(keyword => (
-                <p key={keyword} className="p-4">
-                  {keyword}
-                </p>
-              ))}
-            </div>
-          )}
+          <p onClick={() => toggleSection({ keywordTitle: item.keywordTitle, index })} className="flex items-center space-x-3 cursor-pointer mt-2">
+            {openSections.find(section => section.keywordTitle === item.keywordTitle && section.index) ? <FaMinus className="bg-gray-700 p-1 rounded" /> : <FaPlus className="bg-gray-700 p-1 rounded" />}
+            <span> {item.keywordTitle}</span>
+          </p>
+
+
+          {openSections.find(section => section.keywordTitle === item.keywordTitle && section.index) && <div className="my-5">
+            {item.keywords.map(keyword => (
+              <div key={keyword} className="mt-1 flex items-center space-x-3">
+                <input type={InputTypes.includes(item.keywordTitle) ? "checkbox" : "radio"} id={keyword} name={item.keywordTitle} value={keyword} className={` appearance-none w-4 h-4 border border-gray-300 checked:bg-blue-500 checked:border-transparent ${InputTypes.includes(item.keywordTitle) ? "rounded" : "rounded-full"}`} />
+                <label className="cursor-pointer" id={keyword} htmlFor={keyword}>{keyword}</label>
+              </div>
+            ))}
+          </div>}
         </div>
       ))}
     </div>
