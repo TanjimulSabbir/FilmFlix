e7aefd09b8a47bf2cc41193dc3f7db3166c0a279
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-30 03:27:20 +0600

    working on movies:filter UI

--- src/Pages/Movies/MoviesHome.jsx
+++ src/Pages/Movies/MoviesHome.jsx
@@ -26,32 +26,35 @@ export default function MoviesHome() {
     { "keywordTitle": "Sort By", "keywords": ["Popular", "New Releases", "Recently Added", "IMDb Rating"] },
     { "keywordTitle": "Genres", "keywords": genres },
     { "keywordTitle": "New Releases", "keywords": ["Last 3 Months", "Last 6 Months", "Last 12 Months"] },
-    { "keywordTitle": "MPAA Rating", "keywords": ["G", "PG", "PG-13", "R", "X R", "NC-17", "Untrated"] },
-    { "keywordTitle": "IMDb Rating", "keywords": ["9.0 or Higher", "8.0 or Higher", "7.0 or Higher", "6.0 or Higher", "5.0 or Higher"] }
+    { "keywordTitle": "MPAA Rating", "keywords": ["G", "PG", "PG-13", "R", "X-R", "NC-17", "Untrated"] },
+    { "keywordTitle": "IMDb Rating", "keywords": ["9.0 or Higher", "8.0 or Higher", "7.0 or Higher", "6.0 or Higher", "5.0 or Higher"] },
+    { "keywordTitle": "Metascore", "keywords": ["90 or Higher", "80 or Higher", "70 or Higher", "60 or Higher", "50 or Higher"] }
   ];
 
   const InputTypes = ["Genres"]
 
   return (
-    <div className="pt-24 container mx-auto">
-      {genresData.map((item, index) => (
-        <div key={item.keywordTitle} className="relative">
-          <p onClick={() => toggleSection({ keywordTitle: item.keywordTitle, index })} className="flex items-center space-x-3 cursor-pointer mt-2">
-            {openSections.find(section => section.keywordTitle === item.keywordTitle && section.index) ? <FaMinus className="bg-gray-700 p-1 rounded" /> : <FaPlus className="bg-gray-700 p-1 rounded" />}
-            <span> {item.keywordTitle}</span>
-          </p>
+    <div className="pt-24">
+      <div className="pt-14 pl-3 sm:pl-5 md:pl-7 h-screen w-[300px] rounded">
+        {genresData.map((item, index) => (
+          <div key={item.keywordTitle} className="relative">
+            <p onClick={() => toggleSection({ keywordTitle: item.keywordTitle, index })} className="flex items-center space-x-5 cursor-pointer mt-5 uppercase text-gray-400 font-bold">
+              {openSections.find(section => section.keywordTitle === item.keywordTitle && section.index) ? <FaMinus className="bg-gray-400 p-1 rounded font-bold text-black" /> : <FaPlus className="bg-gray-400 p-1 rounded font-bold text-black" />}
+              <span> {item.keywordTitle}</span>
+            </p>
 
 
-          {openSections.find(section => section.keywordTitle === item.keywordTitle && section.index) && <div className="my-5">
-            {item.keywords.map(keyword => (
-              <div key={keyword} className="mt-1 flex items-center space-x-3">
-                <input type={InputTypes.includes(item.keywordTitle) ? "checkbox" : "radio"} id={keyword} name={item.keywordTitle} value={keyword} className={` appearance-none w-4 h-4 border border-gray-300 checked:bg-blue-500 checked:border-transparent ${InputTypes.includes(item.keywordTitle) ? "rounded" : "rounded-full"}`} />
-                <label className="cursor-pointer" id={keyword} htmlFor={keyword}>{keyword}</label>
-              </div>
-            ))}
-          </div>}
-        </div>
-      ))}
+            {openSections.find(section => section.keywordTitle === item.keywordTitle && section.index) && <div className="my-3">
+              {item.keywords.map(keyword => (
+                <div key={keyword} className="mt-1 flex items-center space-x-5">
+                  <input type={InputTypes.includes(item.keywordTitle) ? "checkbox" : "radio"} id={keyword} name={item.keywordTitle} value={keyword} className={`cursor-pointer appearance-none w-4 h-4 border border-gray-300 checked:bg-blue-500 checked:border-transparent ${InputTypes.includes(item.keywordTitle) ? "rounded" : "rounded-full"}`} />
+                  <label className="cursor-pointer" id={keyword} htmlFor={keyword}>{keyword}</label>
+                </div>
+              ))}
+            </div>}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
