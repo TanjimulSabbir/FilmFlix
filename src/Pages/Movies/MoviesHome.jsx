df3a5269740f61750246ebbb633e13b9970cc2e2
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-03-30 00:48:44 +0600

    working on movies

--- src/Pages/Movies/MoviesHome.jsx
+++ src/Pages/Movies/MoviesHome.jsx
@@ -1,18 +1,40 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 
 export default function MoviesHome() {
-  const [isOpen, setIsOpen] = useState(false);
+  const genres = useSelector(state => state.movieData.genresData.map(genre => genre.name));
+  const [openSections, setOpenSections] = useState(Array(genres.length).fill(false));
+
+  const toggleSection = (index) => {
+    const updatedSections = [...openSections];
+    updatedSections[index] = !updatedSections[index];
+    setOpenSections(updatedSections);
+  };
+
+  const genresData = [
+    { "keywordTitle": "Sort By", keywords: ["Popular", "New Releases", "Recently Added", "IMDb Rating"] },
+    { "keywordTitle": "Genres", "keywords": genres },
+    { "keywordTitle": "New Releases", "keywords": ["Last 3 Months", "Last 6 Months", "Last 12 Months"] },
+    { "keywordTitle": "MPAA Rating", "keywords": ["G", "PG", "PG-13", "R", "X R", "NC-17", "Untrated"] },
+    { "keywordTitle": "IMDb Rating", "keywords": ["9.0 or Higher", "8.0 or Higher", "7.0 or Higher", "6.0 or Higher", "5.0 or Higher"] }
+  ];
+
   return (
     <div className="pt-24 container mx-auto">
-      {/* Button to toggle the collapse */}
-      <button onClick={() => setIsOpen(!isOpen)} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-        Toggle Collapse
-      </button>
-
-      {/* Collapsible content */}
-      <div className={`absolute transition-all ease-in-out duration-500 ${isOpen ? 'py-8 opacity-100' : ' py-0 opacity-0'}`}>
-        <p className="p-4">Collapsible content goes here...</p>
-      </div>
-    </div >
-  )
+      {genresData.map((item, index) => (
+        <div key={item.keywordTitle}>
+          <button onClick={() => toggleSection(index)} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+            {item.keywordTitle}
+          </button>
+          <div className={`transition-all flex flex-col space-y-2 ease-in-out duration-500 ${openSections[index] ? 'py-8 opacity-100' : 'max-h-0 py-0 opacity-0'}`}>
+            {item.keywords.map(keyword => (
+              <p key={keyword} className="p-4">
+                {keyword}
+              </p>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/components/shared/header/Genres.jsx
+++ src/components/shared/header/Genres.jsx
@@ -1,18 +1,25 @@
+import { useDispatch } from "react-redux";
 import { useGetAllDataSlashQuery } from "../../../Redux/Features/Api/movieApi";
 import Loading from "../../accessories/Loading";
 import { FaCaretRight } from "react-icons/fa6";
 import { Link } from "react-router-dom"
+import { useEffect } from "react";
+import { addGenresData } from "../../../Redux/Features/movies/moviesSlice";
 
 function Genres() {
     const { data, isloading, isError } = useGetAllDataSlashQuery({ type: "genre", id: "", keyword: "movie/list" });
 
-    if (isloading) return <Loading />
-
-
+    const disPatch = useDispatch();
 
     const handleGenres = (id) => {
 
     }
+
+    useEffect(() => {
+        if (data?.genres?.length > 0) disPatch(addGenresData(data.genres))
+    }, [data, disPatch])
+
+    if (isloading) return <Loading />
 
     console.log(data?.genres, "from genres")
     return (
